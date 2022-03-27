0ba1bb26e21101960b08f21a403ca2cdb271bea4
Masnawi Rahmat <masnawi.rahmat@gmail.com>
2022-03-27 20:30:56 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 # in Generation SG/Temasek Poly SGUPMSCT Cloud Support and DevOps Bootcamp cohort no 4 Yr 2022
 # this terraform is created with learning purpose more than optimisation in mind
 # this terraform code with variables create an 'imaginary' company (company_x) and its departments as directory and subdirectories with randomnised department head name created in respective txt files
-# objectives of this terraform code is to give you the basic understanding of how terraform with variables work by using the 'local_file' & 'random_pet' resources to:
+# objective of this terraform code is to give you the basic understanding of how terraform with variables work by using the 'local_file' & 'random_pet' resources to:
 # (1) create directory and subdirectories in your local machine
 # (2) create txt files (with randomly created 'content(s)') in the subdirectories
 # please substitute 'your_user_name' in 'default' (ie. "foo") in variable.tf with your own user name
